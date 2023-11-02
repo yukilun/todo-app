153691d9c1a04eaf4247aa440b41c55b3985ff06
yukilun <102889935+yukilun@users.noreply.github.com>
2023-11-01 23:25:04 -0700

    add focus indicator

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,28 +3,30 @@
         <template #item="{ element, index }">
             <div v-show="todosStore.filter == 'All' || (todosStore.filter == 'Active' && !element.isCompleted) || (todosStore.filter == 'Completed' && element.isCompleted)"
                 class="todo px-5 py-4 md:px-6 md:py-[17px] border-b-[1px] border-grayish-blue-200 dark:border-grayish-blue-900 flex items-center gap-3 md:gap-5 group">
-                <label class="flex-grow overflow-hidden flex items-center cursor-pointer gap-3 md:gap-5 group">
+                <div class="flex-grow overflow-hidden flex items-center cursor-pointer gap-3 md:gap-5 group">
                     <!-- Todo Completion Toggle Button -->
-                    <span class="block flex-shrink-0 h-5 w-5 md:h-6 md:w-6 p-[1px] rounded-full transition-custom"
-                        :class="element.isCompleted ? 'bg-gradient-to-br from-sky-blue to-orchid'
-                            : 'bg-grayish-blue-50 dark:bg-grayish-blue-950 group-hover:bg-gradient-to-br group-hover:from-sky-blue group-hover:to-orchid'">
+                    <div class="focus-within:outline-auto m-0.5">
+                        <label
+                            class="block flex-shrink-0 h-5 w-5 md:h-6 md:w-6 p-[1px] rounded-full transition-custom cursor-pointer"
+                            :class="element.isCompleted ? 'bg-gradient-to-br from-sky-blue to-orchid'
+                                : 'bg-grayish-blue-50 dark:bg-grayish-blue-950 group-hover:bg-gradient-to-br group-hover:from-sky-blue group-hover:to-orchid focus-within:bg-gradient-to-br focus-within:from-sky-blue focus-within:to-orchid'">
 
-                        <span class="h-full w-full rounded-full flex justify-center items-center"
-                            :class="element.isCompleted ? 'bg-gradient-to-br from-sky-blue to-orchid' : 'bg-white dark:bg-blue-800'">
-                            <IconCheck v-if="element.isCompleted" />
-                        </span>
-                        <input type="checkbox" v-model="element.isCompleted" class="hidden">
-                    </span>
+                            <span class="h-full w-full rounded-full flex justify-center items-center"
+                                :class="element.isCompleted ? 'bg-gradient-to-br from-sky-blue to-orchid' : 'bg-white dark:bg-blue-800'">
+                                <IconCheck v-if="element.isCompleted" />
+                            </span>
+                            <input type="checkbox" v-model="element.isCompleted" class="h-0 w-0 absolute -left-100vw">
+                        </label>
+                    </div>
                     <!-- Todo Task -->
                     <span class="flex-grow text-normal whitespace-nowrap text-ellipsis overflow-hidden"
                         :class="element.isCompleted ? 'line-through text-grayish-blue-200 dark:text-grayish-blue-900' : 'text-grayish-blue-800 dark:text-grayish-blue-300'">
                         {{ element.task }}
                     </span>
-                </label>
+                </div>
                 <!-- Todo Removal Button -->
-                <button type="button" :aria-label="`remove todo: ${element.task}`"
-                    @click="() => todosStore.remove(index)" 
-                    class="transition-custom hide-for-hover-only group-hover:opacity-50 hover:!opacity-100">
+                <button type="button" :aria-label="`remove todo: ${element.task}`" @click="() => todosStore.remove(index)"
+                    class="transition-custom hide-for-hover-only group-hover:opacity-50 hover:!opacity-100 focus:!opacity-100">
                     <IconCross class="h-[14px] w-[14px] md:h-5 md:w-5" />
                 </button>
 
